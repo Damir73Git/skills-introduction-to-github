9657c91048c3ac50630076b75d3fb4aaf88e74f3
Damir73Git <gilmutdinov.doom@gmail.com>
2024-12-11 19:13:51 +0300

    Add files via upload

--- my_list..py
+++ my_list..py
@@ -13,32 +13,31 @@ while 1 > 0:
     my_list10 = my_list[9]
     my_list11 = my_list[10]
     my_list12 = my_list[11]
-    for my_list in range(1):
-        if my_list1 > i:
-            print(my_list1)
-            if my_list2 >= i:
-                print(my_list2)
-                if my_list3 >= i:
-                    print(my_list3)
-                    if my_list4 >= i:
-                        print(my_list4)
-                        if my_list5 >= i:
-                            print(my_list5)
-                            if my_list6 >= i:
-                                print(my_list6)
-                                if my_list7 >= i:
-                                    print(my_list7)
-                                    if my_list8 >= i:
-                                        print(my_list8)
-                                        if my_list9 >= i:
-                                            print(my_list9)
-                                            if my_list10 >= i:
-                                                print(my_list10)
-                                                if my_list11 >= i:
-                                                    print(my_list11)
-                                                    if my_list12 >= i:
-                                                        print(my_list12)
-                                                        continue
+    if my_list1 > i:
+        print(my_list1)
+        if my_list2 >= i:
+            print(my_list2)
+            if my_list3 >= i:
+                print(my_list3)
+                if my_list4 >= i:
+                    print(my_list4)
+                    if my_list5 >= i:
+                        print(my_list5)
+                        if my_list6 >= i:
+                            print(my_list6)
+                            if my_list7 >= i:
+                                print(my_list7)
+                                if my_list8 >= i:
+                                    print(my_list8)
+                                    if my_list9 >= i:
+                                        print(my_list9)
+                                        if my_list10 >= i:
+                                            print(my_list10)
+                                            if my_list11 >= i:
+                                                print(my_list11)
+                                                if my_list12 >= i:
+                                                    print(my_list12)
+                                                    continue
     else:
         if my_list1 < i:
             print(my_list1)
