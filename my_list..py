ef0e9dbef64eaa54b88af356931612f476a0f0dc
Damir73Git <gilmutdinov.doom@gmail.com>
2024-12-13 16:46:28 +0300

    Update my_list..py

--- my_list..py
+++ my_list..py
@@ -1,68 +1,12 @@
-while 1 > 0:
-    my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while 1 > 0:    
     i = 0
-    my_list1 = my_list[0]
-    my_list2 = my_list[1]
-    my_list3 = my_list[2]
-    my_list4 = my_list[3]
-    my_list5 = my_list[4]
-    my_list6 = my_list[5]
-    my_list7 = my_list[6]
-    my_list8 = my_list[7]
-    my_list9 = my_list[8]
-    my_list10 = my_list[9]
-    my_list11 = my_list[10]
-    my_list12 = my_list[11]
+    my_list = int(input("Введите число: "))
     if my_list1 > i:
-        print(my_list1)
-        if my_list2 >= i:
-            print(my_list2)
-            if my_list3 >= i:
-                print(my_list3)
-                if my_list4 >= i:
-                    print(my_list4)
-                    if my_list5 >= i:
-                        print(my_list5)
-                        if my_list6 >= i:
-                            print(my_list6)
-                            if my_list7 >= i:
-                                print(my_list7)
-                                if my_list8 >= i:
-                                    print(my_list8)
-                                    if my_list9 >= i:
-                                        print(my_list9)
-                                        if my_list10 >= i:
-                                            print(my_list10)
-                                            if my_list11 >= i:
-                                                print(my_list11)
-                                                if my_list12 >= i:
-                                                    print(my_list12)
-                                                    continue
+        print('+')
+        continue     
     else:
-        if my_list1 < i:
-            print(my_list1)
-            if my_list2 < i:
-                print(my_list2)
-                if my_list3 < i:
-                    print(my_list3)
-                    if my_list4 < i:
-                        print(my_list4)
-                        if my_list5 < i:
-                            print(my_list5)
-                            if my_list6 < i:
-                                print(my_list6)
-                                if my_list7 < i:
-                                    print(my_list7)
-                                    if my_list8 < i:
-                                        print(my_list8)
-                                        if my_list9 < i:
-                                            print(my_list9)
-                                            if my_list10 < i:
-                                                print(my_list10)
-                                                if my_list11 < i:
-                                                    print(my_list11)
-                                                    if my_list12 < i:
-                                                        print(my_list12)
+        print('-')
     break
 
 
