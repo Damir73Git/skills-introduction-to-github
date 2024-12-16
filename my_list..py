f41ae1b67e3b345065924f8891903b4dfc00d22c
Damir73Git <gilmutdinov.doom@gmail.com>
2024-12-16 21:17:03 +0300

    Add files via upload

---
+++ my_list..py
@@ -0,0 +1,24 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while 1 > 0 :
+    i = 0
+    my_list = int(input("Введите число: "))
+    if my_list == i:
+        print('+')
+        continue
+    if my_list > i:
+        print('+')
+        continue
+    else:
+        print('-')
+    break
+
+
+
+
+
+
+
+
+
+
+
