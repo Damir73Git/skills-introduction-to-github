cc770dede0d3ad250b6a4dc5b54968d4a7fa9dd7
Damir73Git <gilmutdinov.doom@gmail.com>
2025-01-01 21:49:59 +0300

    Update my_list..py

--- my_list..py
+++ my_list..py
@@ -1,16 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-while 1 > 0 :
-    i = 0
-    my_list = int(input("Введите число: "))
-    if my_list == i:
-        print('+')
-        continue
-    if my_list > i:
-        print('+')
-        continue
-    else:
-        print('-')
-    break
+i = 0
+while len(my_list > i:
+    print(my_list[i])
+    i = i + 1        
+    if my_list[i] < 0:
+        my_list.remove(my_list[i])
+        break
 
 
 
